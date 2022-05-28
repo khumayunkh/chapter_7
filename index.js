534318a562f1caf21d3d228458f8f8903964a37c
khumayunkh <hasanovhumayun15@gmail.com>
2022-05-28 19:35:54 +0500

    copy

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ console.log(i[2]) // 22
 let x = [12, {name: "Humoyun", age:19}]
 console.log(x[1].name) // Humoyun
 
+let o  = Array.from(x)
+o[0] = 14
+console.log(x[0]) //12
+console.log(o) // [12, {name:'Humoyun', age:19}]
 let a = "humoyun"
 let b = 'HUMOYUN'
 
@@ -24,10 +28,19 @@ function upper(a,b){
 
 upper(a,b)
 
-let original = [1, 2, 3, 4]
+let original = [1, 2, 3, 4, 4, 7, 7, 2, 3]
 let copy = [...original]
 console.log(copy[1]) // 2
 copy[1] = 3
 console.log(copy[1]) // change to 3 
 console.log(original[1]) // 2
+
+let copy_2 = [...new Set(original)] // [1, 2, 3, 7 ]
+console.log(copy_2)  // no repetition in items
+
+let n = new Array(1,2,3,4) // create a new array === let n = [ 1, 2, 3, 4]
+console.log(n) // [1, 2, 3, 4]
+
+original[6+1] = 34 // item 7 = 34
+console.log(original[7])
 
