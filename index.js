f6a9242ea664017fa8000ef356803d56c0e8b8a2
khumayunkh <hasanovhumayun15@gmail.com>
2022-05-28 21:47:49 +0500

    methods

--- index.js
+++ index.js
@@ -1,46 +1,87 @@
-console.log('chapter_7')
+// console.log('chapter_7')
 
-// let x = []  // array without items
-// let y = [1.2, 'hello', 3, ]
-// console.log(y.length) // 3
-// let z = [1, 4, 3, ,]
-// console.log(z.length) // 4
+// // let x = []  // array without items
+// // let y = [1.2, 'hello', 3, ]
+// // console.log(y.length) // 3
+// // let z = [1, 4, 3, ,]
+// // console.log(z.length) // 4
 
-let u = 12
-let i = [u, u-10, u+10, u/2]
-console.log(i[2]) // 22
+// let u = 12
+// let i = [u, u-10, u+10, u/2]
+// console.log(i[2]) // 22
 
-let x = [12, {name: "Humoyun", age:19}]
-console.log(x[1].name) // Humoyun
+// let x = [12, {name: "Humoyun", age:19}]
+// console.log(x[1].name) // Humoyun
 
-let o  = Array.from(x)
-o[0] = 14
-console.log(x[0]) //12
-console.log(o) // [12, {name:'Humoyun', age:19}]
-let a = "humoyun"
-let b = 'HUMOYUN'
+// let o  = Array.from(x)
+// o[0] = 14
+// console.log(x[0]) //12
+// console.log(o) // [14, {name:'Humoyun', age:19}]
+// let a = "humoyun"
+// let b = 'HUMOYUN'
 
-function upper(a,b){
-    let y = a.toUpperCase()
-    let d = b.toUpperCase()
-    console.log(y===d) // true
+// function upper(a,b){
+//     let y = a.toUpperCase()
+//     let d = b.toUpperCase()
+//     console.log(y===d) // true
+// }
+
+// upper(a,b)
+
+// let original = [1, 2, 3, 4, 4, 7, 7, 2, 3]
+// let copy = [...original]
+// console.log(copy[1]) // 2
+// copy[1] = 3
+// console.log(copy[1]) // change to 3 
+// console.log(original[1]) // 2
+
+// let copy_2 = [...new Set(original)] // [1, 2, 3, 7 ]
+// console.log(copy_2)  // no repetition in items
+
+// let n = new Array(1,2,3,4) // create a new array === let n = [ 1, 2, 3, 4]
+// console.log(n) // [1, 2, 3, 4]
+
+// original[6+1] = 34 // item 7 = 34
+// console.log(original[7])
+
+// let x = {} //obj
+// x[1] = 3 // key:1, value:3
+// x[0] = 2 // key:0, value:2
+// console.log(x) // {o:2, 1:3}
+
+let y = new Array(2,3,4,4)
+y.push(5) // add item 5
+console.log(y) // [2, 3, 4, 4, 5] // length = 5
+y.pop() // delete last item 5
+console.log(y) // [2, 3, 4, 4] // length = 4
+console.log(y)
+let x = [...new Set(y)]
+console.log(x)
+let z = [...y]
+console.log(z)
+let t = Array.from(y)
+console.log(t)
+y.length = 3
+console.log('yyyyyy ', y) // 2, 3, 4
+y.length = 5
+console.log('yyyyyyyyyyy ', y) // 2, 3, 4 and empty array
+
+let obj = Object.create({},{
+    name:{
+        value: "humoyun"
+    }
+})
+
+console.log(obj.name)
+
+let ob = {
+    name:"humo",
+    age:20,
+    form:10
 }
+let obj_2 = Object.assign({},ob)
+console.log(obj_2)
+let obj_3 = {...ob}
+console.log(obj_3)
 
-upper(a,b)
-
-let original = [1, 2, 3, 4, 4, 7, 7, 2, 3]
-let copy = [...original]
-console.log(copy[1]) // 2
-copy[1] = 3
-console.log(copy[1]) // change to 3 
-console.log(original[1]) // 2
-
-let copy_2 = [...new Set(original)] // [1, 2, 3, 7 ]
-console.log(copy_2)  // no repetition in items
-
-let n = new Array(1,2,3,4) // create a new array === let n = [ 1, 2, 3, 4]
-console.log(n) // [1, 2, 3, 4]
-
-original[6+1] = 34 // item 7 = 34
-console.log(original[7])
 
